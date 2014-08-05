cb47079ebb5e7736c1af91e12ab11287211d0433
Antonino Porcino <nino.porcino@gmail.com>
2014-08-05 09:23:08 +0200

    Added form.FormController

--- AngularJS/AngularJS/Classes/Attributes.cs
+++ AngularJS/AngularJS/Classes/Attributes.cs
@@ -9,40 +9,66 @@ using System.Reflection;
 using System.Diagnostics;
 
 namespace AngularJS
-{       
+{          
+   /// <summary>
+   /// $compile.directive.Attributes 
+   /// A shared object between directive compile / linking functions which contains normalized DOM element attributes.    
+   /// </summary>
    [Imported]
    public sealed class Attributes
    {
       [IntrinsicProperty]
       public string this[string key] { get {return "";}  set {}  }
             
-      public JsDictionary Attr 
+      public JsDictionary attr 
       {
          [InlineCode("{this}.$attr")] get { return null; }
       }
+
+      /// <summary>
+      /// Adds the CSS class value specified by the classVal parameter to the element. If animations are enabled then an animation will be triggered for the class addition.
+      /// </summary>
+      [ScriptName("$addClass")] public void addClass(string classVal) { }
+
+      /// <summary>
+      /// Removes the CSS class value specified by the classVal parameter from the element. If animations are enabled then an animation will be triggered for the class removal.
+      /// </summary>
+      [ScriptName("$removeClass")] public void removeClass(string classVal) { }
       
+      /// <summary>
+      /// Adds and removes the appropriate CSS class values to the element based on the difference between the new and old CSS class values (specified as newClasses and oldClasses).
+      /// </summary>
+      [ScriptName("$updateClass")] public void updateClass(string newClasses, string oldClasses) { }
+
+      /// <summary>
+      /// Observe an interpolated attribute. The observer will never be called, if given attribute is not interpolated.
+      /// </summary>
+      [ScriptName("$observe")] public Action observe(string key, Action<string> function) { return null; }
+
+      /// <summary>
+      /// Set DOM element attribute value.
+      /// </summary>
+      [InlineCode("{this}.$set({key},{value})")]
+      public void set(string key, string value) {}      
+      
+      /// <summary>
+      /// Set DOM element attribute value.
+      /// </summary>
+      [InlineCode("{this}.$set({key},{value})")]
+      public void set(string key, bool value) {}      
+
+      [ScriptName("$normalize")] public string normalize(string name) { return ""; }
+
+      /*
+      // these seems to be obsolete
       public object Element  // TODO: fix to jElement or Angular.Element
       {
          [InlineCode("{this}.$$element")] get { return null; }
       }
 
-      [InlineCode("{this}.$normalize({name})")]
-      public string Normalize(string name) { return ""; }
-
-      [InlineCode("{this}.$set({key},{value})")]
-      public void Set(string key, string value) {}      
-      
-      [InlineCode("{this}.$set({key},{value})")]
-      public void Set(string key, bool value) {}      
-
       [InlineCode("{this}.$set({key},null)")]
       public void Delete(string key) {}      
-
-      /// <summary>
-      /// Observe an interpolated attribute. The observer will never be called, if given attribute is not interpolated.
-      /// </summary>
-      [InlineCode("{this}.$observe({key},{function})")]
-      public void Observe(string key, Action function) {}
+      */
    }                                  
 }
 
---
+++ AngularJS/AngularJS/Classes/FormController.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+using System.Html;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Serialization;
+using System.Reflection;
+using System.Diagnostics;
+
+namespace AngularJS
+{          
+   /// <summary>
+   /// keeps track of all its controls and nested forms as well as the state of them, such as being valid/invalid or dirty/pristine.
+   /// Each form directive creates an instance of FormController.
+   /// </summary>
+   [Imported]
+   public sealed class FormController
+   {
+
+      /// <summary>
+      /// Rollback all form controls pending updates to the $modelValue.
+      /// </summary>
+      [ScriptName("$rollbackViewValue(")] public void rollbackViewValue() {}
+
+      /// <summary>
+      /// Commit all form controls pending updates to the $modelValue.
+      /// </summary>
+      [ScriptName("$commitViewValue")] public void commitViewValue() {}
+
+      /// <summary>
+      /// Register a control with the form.
+      /// </summary>
+      [ScriptName("$addControl")] public void addControl() {}
+
+      /// <summary>
+      // Deregister a control from the form.
+      /// </summary>
+      [ScriptName("$removeControl")] public void removeControl() {}
+
+      /// <summary>
+      /// Sets the validity of a form control.
+      /// </summary>
+      [ScriptName("$setValidity")] public void setValidity() {}
+
+      /// <summary>
+      /// Sets the form to a dirty state.
+      /// </summary>
+      [ScriptName("$setDirty(")] public void setDirty() {}
+
+      /// <summary>
+      /// Sets the form to its pristine state.
+      /// </summary>
+      [ScriptName("$setPristine")] public void setPristine() {}
+
+      /// <summary>
+      /// Sets the form to its submitted state.
+      /// </summary>
+      public void setSubmitted() {}
+
+      /// <summary>
+      /// True if user has not interacted with the form yet.
+      /// </summary>
+      [ScriptName("$pristine")] public bool pristine;
+
+      /// <summary>
+      /// True if user has already interacted with the form.
+      /// </summary>
+      [ScriptName("$dirty")] public bool dirty;
+
+      /// <summary>
+      /// True if all of the containing forms and controls are valid.
+      /// </summary>
+      [ScriptName("$valid")] public bool valid;
+
+      /// <summary>
+      /// True if at least one containing control or form is invalid.
+      /// </summary>
+      [ScriptName("$invalid")] public bool invalid;
+
+      /// <summary>
+      /// An object hash, containing references to all invalid controls or forms,
+      /// </summary>
+      [ScriptName("$error")] public JsDictionary error;
+   }
+}
+
